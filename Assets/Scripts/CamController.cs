26d0b6a9d96fc8d8ba708f497013eb2533f9d3e6
Nachiket Dabhade <dabhadenachiket@gmail.com>
2020-08-07 12:49:01 -0700

    BuggyPlayerMovement

---
+++ Assets/Scripts/CamController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamController : MonoBehaviour
+{
+    public GameObject[] cameraList;
+    public int currentCamera;
+    void Start()
+    {
+        currentCamera = 0;
+        for (int i = 0; i < cameraList.Length; i++)
+        {
+            cameraList[0].gameObject.SetActive(false);
+        }
+
+        if (cameraList.Length > 0)
+        {
+            cameraList[0].gameObject.SetActive(true);
+        }
+    }
+
+    void Update()
+    {
+        
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            currentCamera++;
+            if (currentCamera < cameraList.Length)
+            {
+                cameraList[currentCamera - 1].gameObject.SetActive(false);
+                cameraList[currentCamera].gameObject.SetActive(true);
+            }
+            else
+            {
+                currentCamera = 0;
+                cameraList[currentCamera].gameObject.SetActive(true);
+                cameraList[cameraList.Length - 1].gameObject.SetActive(false);
+            }
+        }
+    }
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,21 +7,62 @@ public class PlayerController : MonoBehaviour
     public float moveSpeed;
     public float rotationSpeed;
 
-    public Transform relativeTransform;
+    public Camera cam;
 
-    void Update()
+    Transform relativeTransform;
+    int camValue;
+
+    Vector3 localMoveInput;
+    Vector3 rightWorldMovement;
+    Vector3 forwardWorldMovement;
+    Vector3 moveDirection;
+
+    private void Start()
     {
-        Vector3 moveDirection = Vector3.zero;
-        if (Input.GetKey(KeyCode.W)) moveDirection += relativeTransform.forward;
-        if (Input.GetKey(KeyCode.S)) moveDirection += -relativeTransform.forward;
-        if (Input.GetKey(KeyCode.A)) moveDirection += -relativeTransform.right;
-        if (Input.GetKey(KeyCode.D)) moveDirection += relativeTransform.right;
+    }
+    void FixedUpdate()
+    {
+        camValue = cam.GetComponent<CamController>().currentCamera;
+        relativeTransform = cam.GetComponent<CamController>().cameraList[camValue].transform;
+        Debug.Log(relativeTransform.forward);
+        if (camValue == 0)
+        {
+            MoveMainCam();
+        }
 
-        moveDirection.y = 0f;
+        else if (camValue == 1 || camValue == 2)
+        {
+            MoveSideCam();
+        }
+
+    }
+
+    void MoveMainCam()
+    {
+        localMoveInput = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
+
+        rightWorldMovement = relativeTransform.right;
+        forwardWorldMovement = relativeTransform.forward;
+
+        moveDirection = Vector3.ClampMagnitude(rightWorldMovement * localMoveInput.x + forwardWorldMovement * localMoveInput.z, 1);
+
+        moveDirection.y = 0;
 
         transform.position += moveDirection.normalized * moveSpeed * Time.deltaTime;
 
         if (moveDirection != Vector3.zero)
             transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.LookRotation(moveDirection), rotationSpeed * Time.deltaTime);
     }
+
+    void MoveSideCam()
+    {
+        localMoveInput = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
+
+        moveDirection = Vector3.ClampMagnitude(rightWorldMovement * localMoveInput.x, 1);
+        transform.position += moveDirection.normalized * moveSpeed * Time.deltaTime;
+
+        if (moveDirection != Vector3.zero)
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.LookRotation(moveDirection), rotationSpeed * Time.deltaTime);
+    }
+    
 }
