bce4f5af110ac81dac3ccaf60574610c568508a8
Nachiket Dabhade <dabhadenachiket@gmail.com>
2020-08-13 20:30:38 -0700

    Buggy Object Movement

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -10,6 +10,11 @@ public class PlayerController : MonoBehaviour
     public Camera cam;
 
     Transform relativeTransform;
+    [SerializeField]
+    GameObject[] groundElements;
+    Transform[] originalPositions;
+    Transform[] tempPositionsTop;
+    Transform[] tempPositionsSide;
     int camValue;
 
     Vector3 localMoveInput;
@@ -19,48 +24,70 @@ public class PlayerController : MonoBehaviour
 
     private void Start()
     {
+        groundElements = GameObject.FindGameObjectsWithTag("Ground");
+        for (int i = 0; i < groundElements.Length; i++)
+        {
+            Debug.Log(originalPositions[i]);
+            originalPositions[i] = groundElements[i].transform;
+            Debug.Log(originalPositions[i]);
+        }
     }
     void FixedUpdate()
     {
         camValue = cam.GetComponent<CamController>().currentCamera;
         if (camValue == 0)
         {
-            MoveMainCam();
+            MainCam();
+            for (int i = 0; i < groundElements.Length; i++)
+            {
+                groundElements[i].transform.position = originalPositions[i].position;
+            }
         }
 
         else if (camValue == 1 || camValue == 2)
         {
-            MoveSideCam();
+            SideCam();
+            for (int i = 0; i < groundElements.Length; i++)
+            {
+                groundElements[i].transform.position = originalPositions[i].position;
+                tempPositionsSide[i].position = originalPositions[i].position;
+                tempPositionsSide[i].position = new Vector3(0, tempPositionsSide[i].position.y, tempPositionsSide[i].position.z);
+                groundElements[i].transform.position = tempPositionsSide[i].position;
+            }
         }
 
         else if (camValue == 3)
         {
-            MoveTopCam();
+            TopCam();
+            for (int i = 0; i < groundElements.Length; i++)
+            {
+                groundElements[i].transform.position = originalPositions[i].position;
+                tempPositionsTop[i].position = originalPositions[i].position;
+                tempPositionsTop[i].position = new Vector3(tempPositionsTop[i].position.x, 0, tempPositionsTop[i].position.z);
+                groundElements[i].transform.position = tempPositionsTop[i].position;
+            }
         }
     }
 
-    void MoveMainCam()
+    void MainCam()
     {
         if (cam.orthographic)
         {
             cam.orthographic = false;
         }
+
         localMoveInput = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
         relativeTransform = cam.transform;
         rightWorldMovement = relativeTransform.right;
         forwardWorldMovement = relativeTransform.forward;
 
         moveDirection = Vector3.ClampMagnitude(rightWorldMovement * localMoveInput.x + forwardWorldMovement * localMoveInput.z, 1);
-
         moveDirection.y = 0;
 
-        transform.position += moveDirection.normalized * moveSpeed * Time.deltaTime;
-
-        if (moveDirection != Vector3.zero)
-            transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.LookRotation(moveDirection), rotationSpeed * Time.deltaTime);
+        Movement(moveDirection);
     }
 
-    void MoveSideCam()
+    void SideCam()
     {
         if (!cam.orthographic)
         {
@@ -70,13 +97,10 @@ public class PlayerController : MonoBehaviour
         relativeTransform = cam.transform;
         rightWorldMovement = relativeTransform.right;
         moveDirection = Vector3.ClampMagnitude(rightWorldMovement * localMoveInput.x, 1);
-        transform.position += moveDirection.normalized * moveSpeed * Time.deltaTime;
-
-        if (moveDirection != Vector3.zero)
-            transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.LookRotation(moveDirection), rotationSpeed * Time.deltaTime);
+        Movement(moveDirection);
     }
 
-    void MoveTopCam()
+    void TopCam()
     {
         localMoveInput = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
         relativeTransform = cam.transform;
@@ -84,9 +108,14 @@ public class PlayerController : MonoBehaviour
         forwardWorldMovement = relativeTransform.up;
 
         moveDirection = Vector3.ClampMagnitude(rightWorldMovement * localMoveInput.x + forwardWorldMovement * localMoveInput.z, 1);
-
         moveDirection.y = 0;
 
+        Movement(moveDirection);
+
+    }
+
+    void Movement(Vector3 moveDirection)
+    {
         transform.position += moveDirection.normalized * moveSpeed * Time.deltaTime;
 
         if (moveDirection != Vector3.zero)
